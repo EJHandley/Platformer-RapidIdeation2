3182b0a10767752f4dd45fcbd31401edc3918a60
Elliot <71550907+EJHandley@users.noreply.github.com>
2020-11-10 00:07:06 +0000

    Mechanics Update
    Added Phobot Health and Health Bar
    Added Phobot Attack
    Added Enemy Health
    Added Enemy Attack

---
+++ Assets/Enemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 1;
+    public float radius = 1f;
+
+    public GameObject deathEffect;
+    public Phobot phobotHit;
+    public Transform phobotPosition;
+
+    public void Awake()
+    {
+        phobotPosition = GameObject.Find("Phobot").transform;
+        phobotHit = GameObject.Find("Phobot").GetComponent<Phobot>();
+    }
+
+    void Update()
+    {
+        float phobotDistance = Vector2.Distance(phobotPosition.position, transform.position);
+        if (phobotDistance <= radius)
+        {
+            phobotHit.TakeDamage(1);
+            Destroy(gameObject);
+        }
+    }
+
+    public void TakeDamage (int damage)
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public void HitPhobot ()
+    {
+
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
---
+++ Assets/Phobot.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Phobot : MonoBehaviour
+{
+    public int health = 3;
+    public float radius = 1f;
+    public GameObject thirdHeart;
+    public GameObject secondHeart;
+    public GameObject firstHeart;
+
+    public void Awake()
+    {
+        thirdHeart = GameObject.Find("ThirdHeart");
+        secondHeart = GameObject.Find("SecondHeart");
+        firstHeart = GameObject.Find("FirstHeart");
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health == 2)
+        {
+            Destroy(thirdHeart);
+        }
+
+        if (health == 1)
+        {
+            Destroy(secondHeart);
+        }
+
+        if (health <= 0)
+        {
+            Destroy(firstHeart);
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
